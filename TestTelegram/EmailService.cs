0e2326a49f024477f09fd3cc4f6719ae2fa3e926
Julie Trushkova <kosyuska@yandex.ru>
2023-10-17 00:31:40 +0300

    Добавьте файлы проекта.

---
+++ TestTelegram/EmailService.cs
@@ -0,0 +1,91 @@
+﻿using MimeKit;
+using System;
+using MailKit.Net.Smtp;
+using MailKit.Net.Imap;
+using MailKit;
+using MailKit.Search;
+
+
+namespace TestTelegram
+{
+    internal class EmailService
+    {
+        //Отправка сообщений от Телеграм бота на почту
+        public static async Task SendMessageAsync(string? textOfMessage)
+        {
+            EmailService emailService = new EmailService();
+            if (textOfMessage == null)
+                textOfMessage = "Message is empty";
+
+            await emailService.SendMailAsync("test@yandex.ru", "test of telegram", textOfMessage);
+
+        }
+        //Проверка писем
+        public static async Task CheckMessageAsync()
+        {
+            EmailService emailService = new EmailService();
+            
+            await emailService.RetrieveMailAsync();
+        }
+        //Отправка письме по SMTP 
+        public async Task SendMailAsync(string email, string subjectOfEmail, string bodyOfMessage)
+        {
+            using MimeMessage emailMessage = new MimeMessage();
+
+            emailMessage.From.Add(new MailboxAddress("Test", "Test@yandex.ru"));
+            emailMessage.To.Add(new MailboxAddress("", email));
+            emailMessage.Subject = subjectOfEmail;
+            emailMessage.Body = new TextPart(MimeKit.Text.TextFormat.Html)
+            {
+                Text = bodyOfMessage
+            };
+
+            using (SmtpClient smtpClient = new SmtpClient())
+            {
+                await smtpClient.ConnectAsync("smtp.yandex.ru", 465, true);
+                await smtpClient.AuthenticateAsync("Test@yandex.ru", System.IO.File.ReadAllText("C:\\yandex_smtp.txt"));
+                await smtpClient.SendAsync(emailMessage);
+                await smtpClient.DisconnectAsync(true);
+            }
+        }
+
+        //Подключение к почте через IMAP и получение списка писем
+        public async Task RetrieveMailAsync()
+        {
+            Console.WriteLine("start retrieve");
+            using (ImapClient imapClient = new ImapClient())
+            {
+                await imapClient.ConnectAsync("imap.yandex.ru", 465, true);
+                await imapClient.AuthenticateAsync("Test@yandex.ru", System.IO.File.ReadAllText("C:\\yandex_imap.txt"));
+                IMailFolder inboxFolder = imapClient.Inbox;
+                inboxFolder.Open(FolderAccess.ReadOnly);
+
+                Console.WriteLine($"Name {inboxFolder.Name}");
+                Console.WriteLine($"Count ALL  {inboxFolder.Count}");
+                Console.WriteLine($"Unread {inboxFolder.Unread}");
+                Console.WriteLine($"Recent {inboxFolder.Recent}");
+
+                for (int i = 0; i < inboxFolder.Count; i++)
+                {
+                    MimeMessage message = inboxFolder.GetMessage(i);
+                    Console.WriteLine(message.From.ToString() + " " + message.MessageId.ToString() + " " + message.Subject.ToString());
+                }
+
+                Console.WriteLine("start filter");
+                var query = SearchQuery.FromContains("yandex.ru").And(SearchQuery.NotSeen);
+
+                foreach (UniqueId uid in inboxFolder.Search(query))
+                {
+                    var message = inboxFolder.GetMessage(uid);
+                    Console.WriteLine("[match] {0}: {1}", uid, message.Subject);
+                    //await telegramBotTest.SendTextMessageAsync(messageChat, message.Subject);
+                    int y = 0;
+                }               
+
+                await imapClient.DisconnectAsync(true);
+
+            }
+            Console.WriteLine("end retrieve");
+        }
+    }
+}
---
+++ TestTelegram/UserTelegramBot.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Telegram.Bot.Types;
+using Telegram.Bot;
+using System.Text.Json;
+using Telegram.Bot.Polling;
+
+namespace TestTelegram
+{
+    internal class UserTelegramBot
+    {
+        static ITelegramBotClient telegramBot = new TelegramBotClient(System.IO.File.ReadAllText("C:\\Users\\Julie\\Documents\\Ju\\programming\\Pyxis\\telegram.txt"));
+        static Chat messageChat;
+
+        //Обработка полученных от пользователя сообщений и отправка ответа
+        static async Task HandleUpdateAsync(ITelegramBotClient telegramBotClient, Update update, CancellationToken cancellationToken)
+        {
+            Console.WriteLine(JsonSerializer.Serialize(update) + "\n");
+
+            if (update.Type == Telegram.Bot.Types.Enums.UpdateType.Message)
+            {
+                Message? message = update.Message;
+                messageChat = message.Chat;
+                if (message?.Text?.ToLower() == "/start")
+                {
+                    await telegramBotClient.SendTextMessageAsync(messageChat, "I am ready to start, Julia!");
+                    return;
+                }
+                await telegramBotClient.SendTextMessageAsync(messageChat, "Helloooo1");
+
+                if (message?.Sticker != null)
+                {
+                    await telegramBotClient.SendTextMessageAsync(messageChat, "You sent me a sticker, Julia!");
+                    return;
+                }
+
+                await telegramBotClient.SendTextMessageAsync(messageChat, "Helloooo2");
+
+                await EmailService.SendMessageAsync(message?.Text);
+            }
+
+        }
+
+        //Обработка ошибок
+        static async Task HandleErrorAsync(ITelegramBotClient telegramBotClient, Exception exception, CancellationToken cancellationToken)
+        {
+            Console.WriteLine($"Error: {Newtonsoft.Json.JsonConvert.SerializeObject(exception)}");
+        }
+
+        //запуск телеграмм бота 
+        public static void RunTelegramBot()
+        {
+            Console.WriteLine($"Bot {telegramBot.GetMeAsync().Result.FirstName} of {telegramBot.GetMeAsync().Result.Username} is running");
+
+            CancellationTokenSource cancellationTokenSource = new CancellationTokenSource();
+            CancellationToken cancellationToken = cancellationTokenSource.Token;
+
+            ReceiverOptions receiverOptions = new ReceiverOptions()
+            {
+                AllowedUpdates = { }
+            };
+            
+            telegramBot.StartReceiving(HandleUpdateAsync, HandleErrorAsync, receiverOptions, cancellationToken);
+        }
+
+    }
+}
